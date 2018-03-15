3aa35001f212df7072067aa6dda9bf2799f45eac
rakaarbhp <rakaarbhp@gmail.com>
2018-03-14 22:38:08 -0400

    Add files via upload

--- Python_app/Checkdbconserver.py
+++ Python_app/Checkdbconserver.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import MySQLdb  #Confirm you get an error
+# import MySQLdb  #Confirm you get an error
 
 import pymysql
 pymysql.install_as_MySQLdb()
