012c6f6dbba096d84e66230899bda8125bf318b8
rakaarbhp <rakaarbhp@gmail.com>
2018-03-15 22:58:28 -0400

    Add files via upload

---
+++ Python_app/dbtest.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+# import MySQLdb  #Confirm you get an error
+
+import pymysql
+pymysql.install_as_MySQLdb()
+import MySQLdb
+#import MySQLdb
+
+app = Flask(__name__)
+
+@app.route('/index')
+
+def index_page():
+        return render_template('index.html')
+
+
+@app.route('/showsignup')
+
+def signup_page():
+        return render_template('signup.html')
+
+
+@app.route('/register',methods=["GET","POST"])
+
+class Database:
+
+    host = '10.0.0.2'
+    user = 'admin'
+    password = 'Watha12!'
+    db = 'Skytap'
+
+    def __init__(self):
+        self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
+        self.cursor = self.connection.cursor()
+
+
+#def register_page():
+#    conn = MySQLdb.connect(host="10.0.0.3",
+#                           user = "root",
+#                           passwd = "watha",
+#                           db = "Skytap",
+#                           port = "1249")
+    print (success)
+    try:
+#        c = conn.cursor()
+        return("okay")
+    except Exception as e:
+        return(str(e))
+
+
+
+if __name__ == "__main__":
+        app.run()
